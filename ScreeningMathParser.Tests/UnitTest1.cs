4f43031b0a1eecb399f9c48b23608eed2351dbc1
danthomas <danthomas000@gmail.com>
2014-11-29 09:01:21 +0000

    Initial commit - resharper requires source control for some reason

---
+++ ScreeningMathParser.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ScreeningMathParser.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string expression = "3a2c4";
+            decimal expected = 20M;
+            decimal actual = new Evaluator().Evaluate();
+        }
+    }
+}
