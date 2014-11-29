df83e359644c198f436a0b983565b8ba1248bee1
danthomas <danthomas000@gmail.com>
2014-11-29 16:03:08 +0000

    commit

---
+++ ScreeningMathParser.Tests/EvaluatorTests.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ScreeningMathParser.Tests
+{
+    public partial class EvaluatorTests
+    {
+
+        [TestMethod]
+        public void Evaluate_3a2c4()
+        {
+            const string expression = "3a2c4";
+
+            const decimal expected = 20M;
+            decimal actual = new Evaluator().Evaluate(expression);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Evaluate_32a2d2()
+        {
+            const string expression = "32a2d2";
+
+            const decimal expected = 17M;
+            decimal actual = new Evaluator().Evaluate(expression);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Evaluate_500a10b66c32()
+        {
+            const string expression = "500a10b66c32";
+
+            const decimal expected = 14208M;
+            decimal actual = new Evaluator().Evaluate(expression);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Evaluate_3ae4c66fb32()
+        {
+            const string expression = "3ae4c66fb32";
+
+            const decimal expected = 235M;
+            decimal actual = new Evaluator().Evaluate(expression);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Evaluate_3c4d2aee2a4c41fc4f()
+        {
+            const string expression = "3c4d2aee2a4c41fc4f";
+
+            const decimal expected = 990M;
+            decimal actual = new Evaluator().Evaluate(expression);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
--- ScreeningMathParser/Evaluator.cs
+++ ScreeningMathParser/Evaluator.cs
@@ -1,12 +1,158 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace ScreeningMathParser
 {
+    /// <summary>
+    /// Evaluates mathematical expressions of the type
+    /// 3c4d2aee2a4c41fc4f
+    /// where a = '+', b = '-', c = '*', d = '/', e = '(', f = ')'
+    /// </summary>
     public class Evaluator
     {
+        private readonly string[] _operatorChars = {"a", "b", "c", "d"};
+        private const string leftParen = "e";
+        private const string rightParen = "f";
+
+        /// <summary>
+        /// Returns try if the specified expression contains only 
+        /// digits and the letters a-f, return false otherwise
+        /// </summary>
+        /// <param name="expression"></param>
+        /// <returns></returns>
+        internal bool ValidateChars(string expression)
+        {
+            return Regex.IsMatch(expression, "^[abcdef0-9]+$");
+        }
+
+        /// <summary>
+        /// Splits the specified expression into numeric values and single chars
+        /// </summary>
+        /// <param name="expression"></param>
+        /// <returns></returns>
+        internal string[] Tokenise(string expression)
+        {
+            return Regex.Matches(expression, @"[a]|[b]|[c]|[d]|[e]|[f]|\d+")
+                .Cast<Match>()
+                .Select(m => m.Value)
+                .ToArray(); 
+        }
+
+        /// <summary>
+        /// Builds an Expression Tree from the specified tokens
+        /// </summary>
+        /// <param name="tokens"></param>
+        /// <returns></returns>
+        internal Node BuildExpressionTree(string[] tokens)
+        {
+            int index = 0;
+            return BuildExpressionTree(tokens, ref index);
+        }
+
+        private Node BuildExpressionTree(string[] tokens, ref int index)
+        {
+            Node node = null;
+            OperatorType operatorType = OperatorType.Addition;
+
+            while (index < tokens.Length)
+            {
+                int value;
+                if (Int32.TryParse(tokens[index], out value))
+                {
+                    index++;
+                    if (node == null)
+                    {
+                        node = new ValueNode(value);
+                    }
+                    else
+                    {
+                        node = new OperatorNode(node, operatorType, new ValueNode(value));
+                    }
+                }
+                else if (_operatorChars.Contains(tokens[index]))
+                {
+                    operatorType = GetOperatorType(tokens[index]);
+                    index++;
+                }
+                else if (tokens[index] == leftParen)
+                {
+                    index++;
+
+                    if (node == null)
+                    {
+                        node = BuildExpressionTree(tokens, ref index);
+                    }
+                    else
+                    {
+                        node = new OperatorNode(node, operatorType, BuildExpressionTree(tokens, ref index));
+                    }
+                }
+                else if (tokens[index] == rightParen)
+                {
+                    index++;
+                    return node;
+                }
+            }
+            
+            return node;
+        }
+
+        /// <summary>
+        /// Gets the OperatorType for the specifed @operator
+        /// </summary>
+        /// <param name="operator"></param>
+        /// <returns></returns>
+        internal OperatorType GetOperatorType(string @operator)
+        {
+            switch (@operator)
+            {
+                case "a":
+                    return OperatorType.Addition;
+                case "b":
+                    return OperatorType.Subtraction;
+                case "c":
+                    return OperatorType.Multiplication;
+                case "d":
+                    return OperatorType.Division;
+            }
+
+            throw new Exception(String.Format("Operator {0} not recognised", @operator));
+        }
+
+        /// <summary>
+        /// Evaluates the specified expression and returns the calculated value.
+        /// </summary>
+        /// <param name="expression"></param>
+        /// <returns></returns>
+        public decimal Evaluate(string expression)
+        {
+            if (!ValidateChars(expression))
+            {
+                throw new Exception("Invalid chars found.");
+            }
+
+            string[] tokens = Tokenise(expression);
+
+            if (!ValidateTokens(tokens))
+            {
+                throw new Exception("Invalid tokens found.");
+            }
+
+            Node node = BuildExpressionTree(tokens);
+
+            return node.Value;
+        }
+
+        internal bool ValidateTokens(string[] tokens)
+        {
+            //Must have an odd number of tokens
+            if ((tokens.Length % 2) != 1)
+            {
+                return false;
+            }
+
+            return true;
+        }
     }
 }
